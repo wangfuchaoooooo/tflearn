f9c2bd41a7b8b27ef90f93b2a354c44ef72d0d9f
wangfuchao <wfc117@163.com>
2018-09-08 18:20:01 +0800

    auto encoder

--- autoencoder.py
+++ autoencoder.py
@@ -24,3 +24,32 @@ encoder = tflearn.fully_connected(encoder, 64)
 decoder = tflearn.fully_connected(encoder, 256)
 decoder = tflearn.fully_connected(decoder, 784, activation='sigmoid')
 
+# Regression, with mean square error
+net = tflearn.regression(decoder, optimizer='adam', learning_rate=0.001,
+                         loss='mean_square', metric=None)
+
+# Training the auto encoder
+model = tflearn.DNN(net, tensorboard_verbose=0)
+model.fit(X, X, n_epoch=20, validation_set=(test_X, test_X),
+          run_id="auto_encoder", batch_size=256)
+
+# Encoding the auto encoder
+print("\nTest encoding of X[0]:")
+encoding_model = tflearn.DNN(encoder, session=model.session)
+print(encoding_model.predict([X[0]]))
+
+# Testing the image reconstruction on new data (test set)
+print("\nVisualizing results after being encoded and decoded:")
+testX = tflearn.data_utils.shuffle(test_X)[0]
+# Applying encode and decode over test set
+encode_decode = model.predict(testX)
+# Compare original images with their reconstructions
+f, a = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(10):
+    temp = [[ii, ii, ii] for ii in list(testX[i])]
+    a[0][i].imshow(np.reshape(temp, (28, 28, 3)))
+    temp = [[ii, ii, ii] for ii in list(encode_decode[i])]
+    a[1][i].imshow(np.reshape(temp, (28, 28, 3)))
+f.show()
+plt.draw()
+plt.waitforbuttonpress()
