f6584aba2f48b29114d447d095175685d4afefa5
wangfuchao <wfc117@163.com>
2018-09-16 11:08:50 +0800

    nlp...

--- generate_cityName.py
+++ generate_cityName.py
@@ -7,7 +7,6 @@ from tflearn.data_utils import *
 
 path = "US_Cities.txt"
 if not os.path.isfile(path):
-    context = ssl._create_unverified_context()
     moves.urllib.request.urlretrieve(
         "https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/US_Cities.txt", path)
 
